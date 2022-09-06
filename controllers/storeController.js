e11b593a0fb741075d92144f1a9c0dc7a2eb301c
josh <josh.davis@live.ca>
2022-09-05 23:08:03 -0400

    added flash msg to createStore controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,7 @@
 
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');  // set in Store.js
+
 exports.myMiddleware = (req, res, next) => {
     // req.name = 'Josh';
     // if (req.name === 'Josh') {
@@ -19,6 +22,18 @@ exports.addStore = (req, res) => {
     })
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+exports.createStore = async (req, res) => {
+    // uses the store schema (in Store.js) to create a new Store instance.
+    // only uses the data in req.body that maps to the store schema.  Any other data not included in the store schema, but sent in the request anyway, will be ignored automatically.
+    try {
+        const store = new Store(req.body);  
+        const savedStore = await store.save();
+        console.log('New store created');
+        req.flash('success', `Successfully created ${store.name}.  Care to leave a review?`)
+        res.redirect(`/store/${savedStore.slug}`);
+    }
+    catch(err) {
+        throw Error(err);
+    }
+    
 }
